902c241766a0cd168394c6a428c5dc303ba9ff77
Maxfer <maximus604@gmail.com>
2023-06-06 14:41:10 +0300

    installation of elements and purpose outlets/action

--- HW4Modul2/ViewController.swift
+++ HW4Modul2/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: — IBOutlet
+
+    @IBOutlet var mainView: UIView!
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueLabel: UILabel!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    // MARK: — IBAction
+    
+    @IBAction func redSliderAction() {
+    }
+    
+    @IBAction func greenSliderAction() {
+    }
+    @IBAction func blueSliderAction() {
+    }
 }
 
