430b07c7ec8084d5ed7896f293949fc85f3a6fa4
Maxfer <maximus604@gmail.com>
2023-06-06 15:21:54 +0300

    add logic sliders

--- HW4Modul2/ViewController.swift
+++ HW4Modul2/ViewController.swift
@@ -12,26 +12,49 @@ class ViewController: UIViewController {
     // MARK: — IBOutlet
 
     @IBOutlet var mainView: UIView!
+    
     @IBOutlet var redLabel: UILabel!
     @IBOutlet var redSlider: UISlider!
+    
     @IBOutlet var greenLabel: UILabel!
     @IBOutlet var greenSlider: UISlider!
+    
     @IBOutlet var blueLabel: UILabel!
     @IBOutlet var blueSlider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        blueSlider.minimumTrackTintColor = .blue
+        
+        setupView()
     }
 
     // MARK: — IBAction
     
     @IBAction func redSliderAction() {
+        setupView()
     }
     
     @IBAction func greenSliderAction() {
+        setupView()
     }
     @IBAction func blueSliderAction() {
+        setupView()
+    }
+    
+    private func setupView() {
+        mainView.backgroundColor = UIColor.init(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
     }
 }
 
