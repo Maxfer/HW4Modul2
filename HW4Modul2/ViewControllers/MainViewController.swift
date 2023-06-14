a206eb4e417ef004a6653ce93f25a4739da5274f
Maxfer <maximus604@gmail.com>
2023-06-14 23:19:51 +0300

    code re-indent

--- HW4Modul2/ViewControllers/MainViewController.swift
+++ HW4Modul2/ViewControllers/MainViewController.swift
@@ -12,40 +12,25 @@ protocol SettingsViewControllerDelegate: AnyObject {
 }
 
 final class MainViewController: UIViewController {
-    // MARK: — IB Outlet
-    // @IBOutlet var specialityLabel: UILabel!
-    
-    // MARK: — Public Propereties
-    // let personValue = ""
     
     // MARK: — Private Propereties
     private var redColorView: CGFloat = 1
     private var greenColorView: CGFloat = 1
     private var blueColorView: CGFloat = 1
     
-        
-    // MARK: — Initializrs
-    // init(name: String) {
-    //      self.name = name
-    //}
-    
     // MARK: — Override Methds
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            setColorView()
-        }
-    //    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-    //        super .touchesBegan(touches, with: event)
-    //        view.endEditing(true)
-    //    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setColorView()
+    }
     
-        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-            guard let settingsVC = segue.destination as? SettingsViewController else { return }
-            settingsVC.redColorView = redColorView
-            settingsVC.greenColorView = greenColorView
-            settingsVC.blueColorView = blueColorView
-            settingsVC.delegate = self
-        }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let settingsVC = segue.destination as? SettingsViewController else { return }
+        settingsVC.redColorView = redColorView
+        settingsVC.greenColorView = greenColorView
+        settingsVC.blueColorView = blueColorView
+        settingsVC.delegate = self
+    }
     
     // MARK: — IBAction
     // @IBAction func logInDidTapped() {}
